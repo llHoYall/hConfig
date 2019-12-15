9aa61964a32aef4a5b79b3bc5a109b78d09cf4fa
llHoYall <hoya128@gmail.com>
2019-12-15 20:11:04 +0900

    Updated python

--- src/tool/vscode/vscode_python.ps1
+++ src/tool/vscode/vscode_python.ps1
@@ -1,16 +1,33 @@
 <# Configure VSCode for Python -----------------------------------------------#>
-Write-Host -NoNewline " ==> Config "
-Write-Host -ForegroundColor YELLOW "vscode (Python)"
+function VSCode_Python_Config($version, $requirements) {
+	Write-Host -NoNewline " ==> Config "
+	Write-Host -ForegroundColor YELLOW "vscode (Python)"
 
-<# Config Python Environment#>
-If (Get-Command python -errorAction SilentlyContinue) {
-	pyenv install 3.8.0-amd64
-}
-pyenv local 3.8.0-amd64
-pyenv rehash
+	Write-Host $version
 
-If ((Test-Path ./.venv) -eq $False) {
-	python -m venv .venv
+	<# Config Python Environment#>
+	If (Get-Command pyenv -errorAction SilentlyContinue) {
+		If ($version -eq $null) {
+			pyenv install 3.8.0-amd64
+			pyenv local 3.8.0-amd64
+		} Else {
+			pyenv install $version
+			pyenv local $version
+		}
+		pyenv rehash
+
+		If ((Test-Path ./.venv) -eq $False) {
+			python -m venv .venv
+		}
+		.\.venv\Scripts\activate
+
+		If ($requirements -eq $null) {
+			pip install -r $PSScriptRoot/requirements.txt
+		} Else {
+			pip install -r $requirements
+		}
+	}
 }
-.\.venv\Scripts\activate
-pip install -r $PSScriptRoot/requirements.txt
+
+<# Run Script ----------------------------------------------------------------#>
+VSCode_Python_Config $args[0], $args[1]
