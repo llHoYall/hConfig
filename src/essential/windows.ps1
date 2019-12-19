15824d2873db4d1c34b73ae509b2a4bef00635ed
llHoYall <hoya128@gmail.com>
2019-12-19 22:41:27 +0900

    Updated src/essential/windows

--- src/essential/windows.ps1
+++ src/essential/windows.ps1
@@ -1,26 +1,50 @@
+#Requires -RunAsAdministrator
+
 <# Install Chocolatey --------------------------------------------------------#>
-Write-Host -NoNewline " ==> Install "
-Write-Host -ForegroundColor Yellow "Chocolatey"
-If (Get-Command choco -errorAction SilentlyContinue) {
-	Write-Host -NoNewline "  => Upgrade "
-	Write-Host -ForegroundColor DarkYellow "Chocolatey"
-	choco upgrade -y chocolatey
-} Else {
-  Set-ExecutionPolicy AllSigned -Force; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+function Windows_Install_Chocolatey() {
+	Write-Host -NoNewline " ==> Install "
+	Write-Host -ForegroundColor Yellow "Chocolatey"
+	If (Get-Command choco -errorAction SilentlyContinue) {
+		Write-Host -NoNewline "  => Upgrade "
+		Write-Host -ForegroundColor DarkYellow "Chocolatey"
+		choco upgrade -y chocolatey
+	} Else {
+		Set-ExecutionPolicy AllSigned -Force; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+	}
 }
 
 <# Install Fonts -------------------------------------------------------------#>
-Write-Host -NoNewline " ==> Install "
-Write-Host -ForegroundColor Yellow "Fonts"
-<# Cascadia Code #>
-If (Test-Path C:\Windows\Fonts\Cascadia.ttf) {
-	Write-Host -NoNewline "  => Upgrade "
-	Write-Host -NoNewline -ForegroundColor DarkYellow "Cascadia Code "
-	Write-Host font
+function Windows_Install_Fonts() {
+	Write-Host -NoNewline " ==> Install "
+	Write-Host -ForegroundColor Yellow "Fonts"
+	<# Cascadia Code #>
+	If (Test-Path C:\Windows\Fonts\Cascadia.ttf) {
+		Write-Host -NoNewline "  => Upgrade "
+		Write-Host -NoNewline -ForegroundColor DarkYellow "Cascadia Code "
+		Write-Host font
   choco upgrade -y cascadiacode
-} Else {
-	Write-Host -NoNewline "  => Install "
-	Write-Host -NoNewline -ForegroundColor DarkYellow "Cascadia Code "
-	Write-Host font
-	choco install -y cascadiacode
+	} Else {
+		Write-Host -NoNewline "  => Install "
+		Write-Host -NoNewline -ForegroundColor DarkYellow "Cascadia Code "
+		Write-Host font
+		choco install -y cascadiacode
+	}
 }
+
+<# Install Java Runtime Environment ------------------------------------------#>
+function Windows_Install_JRE() {
+	Write-Host -NoNewline " ==> Install "
+	Write-Host -ForegroundColor Yellow "JRE"
+	If (Get-Command java -errorAction SilentlyContinue) {
+		Write-Host -NoNewline "  => Upgrade "
+		Write-Host -ForegroundColor DarkYellow "JRE "
+		choco upgrade -y javaruntime
+	} Else {
+		choco install -y javaruntime
+	}
+}
+
+<# Run Script ----------------------------------------------------------------#>
+Windows_Install_Chocolatey
+Windows_Install_Fonts
+Windows_Install_JRE
