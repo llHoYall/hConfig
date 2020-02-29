b644d8f73255e995e3f5318807005de6f8d22ff0
llHoYall <hoya128@gmail.com>
2020-02-29 19:44:11 +0900

    #1: Create a project
     * [v] Create a project
     * [v] Configure the project
      + Default UI
      + Icon
      + Shortcut

---
+++ project/src/windows.py
@@ -0,0 +1,52 @@
+import sys
+import os
+import ctypes
+import winreg
+from PyQt5.QtWidgets import QApplication, QWidget, QVBoxLayout, QLabel
+
+
+class Windows(QWidget):
+    def __init__(self, parent=None):
+        super().__init__(parent)
+
+        self.init_ui()
+
+    def init_ui(self):
+        # System Information
+        version_info = QLabel()
+        version = self.get_system_information()
+        version = "System Version Information: {} ({}-{})".format(
+            version[0], version[1], version[2]
+        )
+        if self.check_system_privilege():
+            version += "[admin]"
+        version_info.setText(version)
+
+        self.check_system_privilege()
+
+        # Layout
+        win_vlayout = QVBoxLayout()
+        win_vlayout.addWidget(version_info)
+
+        self.setLayout(win_vlayout)
+
+    def get_system_information(self):
+        key = r"SOFTWARE\Microsoft\Windows NT\CurrentVersion"
+        with winreg.OpenKey(winreg.HKEY_LOCAL_MACHINE, key) as key:
+            product_name = winreg.QueryValueEx(key, r"ProductName")[0]
+            release_id = winreg.QueryValueEx(key, r"ReleaseID")[0]
+            current_build = winreg.QueryValueEx(key, r"CurrentBuild")[0]
+
+        return (product_name, release_id, current_build)
+
+    def check_system_privilege(self):
+        return ctypes.windll.shell32.IsUserAnAdmin() != 0
+
+
+if __name__ == "__main__":
+    APP = QApplication(sys.argv)
+    ex = Windows()
+    ex.setWindowTitle("HoYa's Configurator - Windows")
+    ex.show()
+    sys.exit(APP.exec_())
+
